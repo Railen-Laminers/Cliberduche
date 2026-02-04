8d2ebffc2a9b7e2321c2efe240dbae3586f95aee
Railen-Laminers <railen.laminero@gmai.com>
2026-02-04 12:51:21 +0800

    Resets Scroll every page changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import Homepage from "./pages/homepage/Homepage";
 import Login from "./pages/auth/Login";
-import Projects from "./pages/homepage/Projects"; // import the Projects page
+import Projects from "./pages/homepage/Projects";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* scroll resets on every route change */}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
